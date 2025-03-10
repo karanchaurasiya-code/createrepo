78ad7bb3631455d152c681c82ac0959445ac1c2e
karanchaurasiya-code <karanchaurasiya2003a@gmai.com>
2025-03-10 19:54:56 +0530

    some thing new add just like css file and js file

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = false;
+
+updateBody()
+
+function updateBody() {
+    if(inputEl.checked){
+        bodyEl.style.background = "black";
+    }else{
+        bodyEl.style.background = "white"
+    }
+
+}
+
+inputEl.addEventListener("input", ()=>{
+    updateBody()
+})
